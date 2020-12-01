a54fb8bd069798c536fc9d50a30335b4cb3bafad
Jordan Gedney <jordangedney@gmail.com>
2020-12-01 09:35:46 -0500

    One, part two

--- 2020/src/One.hs
+++ 2020/src/One.hs
@@ -6,9 +6,12 @@ partOne input =
   let (a, b):_ = [(x, y) | x <- input, y <- input, (x + y) == 2020]
   in a * b
 
-partTwo = undefined
+partTwo input =
+  let (a, b, c):_ = [(x, y, z) | x <- input, y <- input, z <- input, (x + y + z) == 2020]
+  in a * b * c
 
 main :: IO ()
 main = do
   input <- map read . lines <$> readFile "inputs/one"
   print (partOne input)
+  print (partTwo input)
--- 2020/test/OneSpec.hs
+++ 2020/test/OneSpec.hs
@@ -3,16 +3,13 @@ module OneSpec (spec) where
 import Test.Hspec
 import One
 
-input = [ 1721
-        , 979
-        , 366
-        , 299
-        , 675
-        , 1456
-        ]
+input = [ 1721 , 979 , 366 , 299 , 675 , 1456]
 
 spec :: Spec
 spec = do
-  describe "partOne" $
+  describe "partOne" $ do
     it "fixes expense reports" $ do
       partOne input `shouldBe` 514579
+
+    it "fixes expense reports, doubly so" $ do
+      partTwo input `shouldBe` 241861950
