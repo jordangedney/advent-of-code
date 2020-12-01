56110d6506c1b919ac72b70ca2b6326e84e2231e
Jordan Gedney <jordangedney@gmail.com>
2020-12-01 09:31:07 -0500

    One, part one

--- 2020/src/One.hs
+++ 2020/src/One.hs
@@ -2,8 +2,13 @@
 
 module One (main, partOne, partTwo) where
 
-partOne = undefined
+partOne input =
+  let (a, b):_ = [(x, y) | x <- input, y <- input, (x + y) == 2020]
+  in a * b
+
 partTwo = undefined
 
 main :: IO ()
-main = undefined
+main = do
+  input <- map read . lines <$> readFile "inputs/one"
+  print (partOne input)
