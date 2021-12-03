d5727fce547b3452d1475bf3e7afdedb37d4df17
Jordan Gedney <jordangedney@gmail.com>
2021-12-03 18:13:34 -0500

    whoops

--- 2021/src/X3.hs
+++ 2021/src/X3.hs
@@ -14,19 +14,19 @@ mostCommon cmp xs = xs
   |> map (foldr (\l (o, z) -> if l == '1' then (o + 1, z) else (o, z + 1)) (0,0))
   |> map (\(oneCount, zeroCount) -> if cmp oneCount zeroCount then '1' else '0')
 
-getRating :: (Int -> Int -> Bool) -> Int -> [(Int, String)] -> [(Int, String)]
-getRating cmp pos xs = map snd xs
+getRating :: (Int -> Int -> Bool) -> Int -> [String] -> [String]
+getRating cmp pos xs = xs
   |> map (drop pos)
   |> rotate
   |> mostCommon cmp
   |> head
-  |> (\c -> [(ind, val) | (ind, val) <- xs, (val !! pos) == c])
+  |> (\c -> [val | val <- xs, (val !! pos) == c])
   |> (\ys -> if length ys == 1 then ys else getRating cmp (pos + 1) ys)
 
 part2 :: [String] -> Int
-part2 xs = zip [0..] xs
+part2 xs = xs
   |> (\x -> (getRating (>=) 0 x, getRating (<) 0 x))
-  |> (\([(_, a)], [(_, b)]) -> binToInt a * binToInt b)
+  |> (\([a], [b]) -> binToInt a * binToInt b)
 
 main :: IO ()
 main = readFile "inputs/3" <&> lines >>> part2 >>= print
