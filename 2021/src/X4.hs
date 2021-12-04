ca750e1ac3a0ae03247b69a9612867f9dffa4233
Jordan Gedney <jordangedney@gmail.com>
2021-12-04 10:40:57 -0500

    deduped

--- 2021/src/X4.hs
+++ 2021/src/X4.hs
@@ -13,92 +13,52 @@ parse xs =
 
 type Game = (Vector Bool, [Int])
 
+boardsWithHits = map (\b -> (fromList (replicate (length b) False), b))
+
+doHits :: Int -> Game -> Vector Bool
+doHits called (hits, board) = zip [0..] board
+  |> (\x -> [(i, True) | (i, v) <- x, v == called])
+  |> (hits //)
+
+runGame :: [Int] -> [Game] -> [(Int, [Game])]
+runGame [] _ = []
+runGame (n:ns) bds =
+  let updated = map (\(h, nums) -> (doHits n (h, nums), nums)) bds
+  in (n, updated) : runGame ns updated
+
+winningInds = [ [0..4]
+              , [numCol..numCol*2 - 1]
+              , [numCol*2..numCol*3 - 1]
+              , [numCol*3..numCol*4 - 1]
+              , [numCol*4..numCol*5 - 1]
+              , take 5 [0,5..]
+              , take 5 [1,6..]
+              , take 5 [2,7..]
+              , take 5 [3,8..]
+              ]
+
+doMath winningBoard =
+  let (winningNum, (called, board)) = winningBoard
+      calledInd = [i | (i, v) <- toList (indexed called), v]
+      uncalled = [x | (i, x) <- zip [0..] board, i `notElem` calledInd]
+  in winningNum * sum uncalled
+
+winner :: Game -> Bool
+winner (game, _ ) =
+  let getIndc xs = [game ! x | x <- xs]
+      check :: [[Bool]]
+      check = map getIndc winningInds
+  in any and check
+
 part1 (numsToCall, boards) =
-  let go = boardsWithHits
-
-      boardsWithHits :: [Game] =
-        map (\b -> (fromList (replicate (length b) False), b)) boards
-
-      doHits :: Int -> Game -> Vector Bool
-      doHits called (hits, board) =
-        zip [0..] board
-        |> (\x -> [(i, True) | (i, v) <- x, v == called])
-        |> (hits //)
-
-      runGame :: [Int] -> [Game] -> [(Int, [Game])]
-      runGame [] _ = []
-      runGame (n:ns) bds =
-        let updated = map (\(h, nums) -> (doHits n (h, nums), nums)) bds
-        in (n, updated) : runGame ns updated
-
-      winner :: Game -> Bool
-      winner (game, _ ) =
-        let winningInds = [ [0..4]
-                          , [numCol..numCol*2 - 1]
-                          , [numCol*2..numCol*3 - 1]
-                          , [numCol*3..numCol*4 - 1]
-                          , [numCol*4..numCol*5 - 1]
-                          , take 5 [0,5..]
-                          , take 5 [1,6..]
-                          , take 5 [2,7..]
-                          , take 5 [3,8..]
-                          ]
-            getIndc xs = [game ! x | x <- xs]
-            check :: [[Bool]]
-            check = map getIndc winningInds
-        in any and check
-
-      findWinner ((lastCalled, games):xs) =
+  let findWinner ((lastCalled, games):xs) =
         let winners = [g | g <- games, winner g]
             won = not $ null winners
         in if won then (lastCalled, head winners) else findWinner xs
-
-      winningBoard = findWinner $ runGame numsToCall boardsWithHits
-
-      doMath =
-        let (winningNum, (called, board)) = winningBoard
-            calledInd = [i | (i, v) <- toList (indexed called), v]
-            uncalled = [x | (i, x) <- zip [0..] board, i `notElem` calledInd]
-        in winningNum * sum uncalled
-
-  in doMath
+  in runGame numsToCall (boardsWithHits boards) |> findWinner |> doMath
 
 part2 (numsToCall, boards) =
-  let go = boardsWithHits
-
-      boardsWithHits :: [Game] =
-        map (\b -> (fromList (replicate (length b) False), b)) boards
-
-      doHits :: Int -> Game -> Vector Bool
-      doHits called (hits, board) =
-        zip [0..] board
-        |> (\x -> [(i, True) | (i, v) <- x, v == called])
-        |> (hits //)
-
-      runGame :: [Int] -> [Game] -> [(Int, [Game])]
-      runGame [] _ = []
-      runGame (n:ns) bds =
-        let updated = map (\(h, nums) -> (doHits n (h, nums), nums)) bds
-        in (n, updated) : runGame ns updated
-
-      winner :: Game -> Bool
-      winner (game, _ ) =
-        let winningInds = [ [0..4]
-                          , [numCol..numCol*2 - 1]
-                          , [numCol*2..numCol*3 - 1]
-                          , [numCol*3..numCol*4 - 1]
-                          , [numCol*4..numCol*5 - 1]
-                          , take 5 [0,5..]
-                          , take 5 [1,6..]
-                          , take 5 [2,7..]
-                          , take 5 [3,8..]
-                          ]
-            getIndc xs = [game ! x | x <- xs]
-            check :: [[Bool]]
-            check = map getIndc winningInds
-        in any and check
-
-      keepTruckn l@(_, b) ((lastCalled, games):xs) =
+  let keepTruckn l@(_, b) ((lastCalled, games):xs) =
         let winners = [g | g <- games, winner g]
             won = length winners == length games
             finally = [g | g@(_, b') <- games, b == b']
@@ -109,16 +69,7 @@ part2 (numsToCall, boards) =
             over = length losers == 1
         in if over then keepTruckn (head losers) xs else findWinner xs
 
-      winningBoard = findWinner $ runGame numsToCall boardsWithHits
-
-      doMath =
-        let (winningNum, (called, board)) = winningBoard
-            calledInd = [i | (i, v) <- toList (indexed called), v]
-            uncalled = [x | (i, x) <- zip [0..] board, i `notElem` calledInd]
-        in winningNum * sum uncalled
-
-  in doMath
+  in runGame numsToCall (boardsWithHits boards) |> findWinner |> doMath
 
 main :: IO ()
 main = readFile "inputs/4" <&> lines >>> parse >>> part2 >>= print
--- main = readFile "4" <&> lines >>> parse >>> part2 >>= print
