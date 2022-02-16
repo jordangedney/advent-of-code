bbf3acb433b74aeeb7d2bf77eeafe9cf335f2e49
Jordan Gedney <jordangedney@gmail.com>
2022-02-15 19:21:28 -0500

    ha

--- 2021/src/X7.hs
+++ 2021/src/X7.hs
@@ -5,7 +5,7 @@ import Lude
 parse xs = splitOn "," xs |> map read
 
 solve :: (Int -> Int -> Int) -> [Int] -> Int
-solve costToCrab crabs = map fuelCost crabs |> foldl1 min
+solve costToCrab crabs = map fuelCost crabs |> minimum
   where fuelCost pos = range 0 (maximum crabs)
                      |> map (\i -> crabs `count` i * costToCrab pos i)
                      |> sum
