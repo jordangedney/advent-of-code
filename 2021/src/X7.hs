f8cc7f01f44df9841f961ad42137a07923aab186
Jordan Gedney <jordangedney@gmail.com>
2022-02-15 19:17:22 -0500

    Im a dolt

--- 2021/src/X7.hs
+++ 2021/src/X7.hs
@@ -4,19 +4,11 @@ import Lude
 
 parse xs = splitOn "," xs |> map read
 
-xs `count` x = length (filter (== x) xs)
-
-solve :: (Int -> Int -> Int) -> [Int] -> (Int, Int)
-solve costToCrab crabs =
-  let possibleCrabs = [0..foldl1 max crabs]
-
-      fuelCost position =
-        sum [crabs `count` i * costToCrab position i | i <- possibleCrabs]
-
-      findMin x@(minPos, minFuel) newPos =
-        if fuelCost newPos < minFuel then (newPos, fuelCost newPos) else x
-
-  in foldl findMin (-1, 999999999999999999) crabs
+solve :: (Int -> Int -> Int) -> [Int] -> Int
+solve costToCrab crabs = map fuelCost crabs |> foldl1 min
+  where fuelCost pos = range 0 (maximum crabs)
+                     |> map (\i -> crabs `count` i * costToCrab pos i)
+                     |> sum
 
 delta a b = max a b - min a b
 
@@ -24,4 +16,4 @@ part1 = solve delta
 part2 = solve (\pos p -> sum (range 0 (delta pos p)))
 
 main :: IO ()
-main = readFile "inputs/7" <&> lines >>> concat >>> parse >>> part1 >>= print
+main = readFile "inputs/7" <&> lines >>> concat >>> parse >>> part2 >>= print
