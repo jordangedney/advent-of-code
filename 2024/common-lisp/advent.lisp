3ceb4eedf6cdc77b6f9ab8b99fd3511d3ff84048
Jordan Gedney <jordangedney@gmail.com>
2024-12-03 19:10:46 -0500

    day3

--- 2024/common-lisp/advent.lisp
+++ 2024/common-lisp/advent.lisp
@@ -74,6 +74,21 @@
           (uiop:run-program curl-cmd :output 'string)
           (a:read-file-into-string input-file-name)))))
 
+(defun strcat (&rest args) (apply #'concatenate 'string args))
+
+(defun split-on (delimiter input)
+  (labels ((recur (d i acc)
+             (if (or (string= d "")
+                     (string= i "")
+                     (> (length d) (length i)))
+                 (list (strcat acc i))
+
+                 (let ((taken (subseq i 0 (length d))))
+                   (if (string= d taken)
+                       (cons acc (recur d (subseq i (length d)) ""))
+                       (recur d (subseq i 1) (strcat acc (subseq i 0 1))))))))
+    (loop for v in (recur delimiter input "") if (string/= v "") collect v)))
+
 ;; day 1 -----------------------------------------------------------------------
 (setf test "
   3   4
@@ -162,3 +177,71 @@
 
 (setq test
 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+
+(defun all-digits? (str) (every #'digit-char-p str))
+
+(defun parse-comma-separated-int (str)
+  (if (= (count #\, str) 1)
+      (match (split-on "," str)
+        ((list a b)
+         (if (and (all-digits? a)
+                  (all-digits? b))
+             (list (parse-integer a) (parse-integer b))))
+        (_ nil))))
+
+(defun parse-mul (input)
+  (->> (split-on "mul(" input)
+       cdr
+       (mapcar (lambda (x) (car (split-on ")" x))))
+       (mapcar #'parse-comma-separated-int)
+       (remove-if-not #'identity)))
+
+(defun part-one (input) (loop for (a b) in (parse-mul input) summing (* a b)))
+
+;; (part-one (get-input 3))
+
+(setq test
+"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
+(defun parse-single-mul (input)
+  (if (>= (length input) 4)
+      (let* ((head (subseq input 0 4))
+             (dropped (subseq input 4)))
+        (multiple-value-bind (split-before split-after)
+            (break-off (lambda (x) (char/= x #\))) dropped)
+          (let ((parsed-val
+                  (if (string= "mul(" head)
+                      (parse-comma-separated-int split-before))))
+            (if parsed-val
+                (list (cons :mul parsed-val) (subseq split-after 1))))))))
+
+(defun parse-token (token result input)
+  (if (>= (length input) (length token))
+      (if (string= (subseq input 0 (length token)) token)
+          (list result (subseq input (length token))))))
+
+(defun apply-parsers (input)
+  (or (parse-single-mul input)
+      (parse-token "do()" :do input)
+      (parse-token "don't()" :dont input)))
+
+(defun parse-input (input)
+  (if (< (length input) 1) nil
+      (match (apply-parsers input)
+        ((list parsed to-do)
+         (cons parsed (parse-input to-do)))
+        (nil
+         (parse-input (subseq input 1))))))
+
+(defun part-two (input)
+  (apply #'+
+         (let ((mul-enabled t)
+               (result nil))
+           (loop for cmd in (parse-input input)
+                 do (match cmd
+                      ((list :mul a b)  (if mul-enabled (push (* a b) result)))
+                      (:do              (setf mul-enabled t))
+                      (:dont            (setf mul-enabled nil))))
+           (reverse result))))
+
+;; (part-two (get-input 3))
